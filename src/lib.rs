f9e0c3beeba23c7472aac57af6aeb9f34b3691a2
Teo <teodor@dahlknutsen.no>
2020-09-13 17:46:51 +0200

    Benchmarking code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,37 @@
+#![feature(test)]
+extern crate test;
+
 mod prime;
 
 #[cfg(test)]
 mod tests {
+    use super::prime::PrimeGenerator;
+    use test::Bencher;
+
+    #[bench]
+    fn generation512(b: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+
+        let mut g = PrimeGenerator::new(512, &mut rng);
+
+        b.iter(|| g.next());
+    }
+
+    #[bench]
+    fn generation128(b: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+
+        let mut g = PrimeGenerator::new(128, &mut rng);
+
+        b.iter(|| g.next());
+    }
+
+    #[bench]
+    fn generation1024(b: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+
+        let mut g = PrimeGenerator::new(1024, &mut rng);
+
+        b.iter(|| g.next());
+    }
 }
