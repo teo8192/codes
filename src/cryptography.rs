166beb809ed763135cf64094adce9d670d5dbb1d
Teo <teodor@dahlknutsen.no>
2020-09-17 18:44:38 +0200

    verify that message is large enough

--- src/cryptography.rs
+++ src/cryptography.rs
@@ -4,7 +4,7 @@ use crate::number_theory::inverse;
 use crate::prime::PrimeGenerator;
 
 struct RSA {
-    keys: (BigUint, BigUint),
+    // keys: (BigUint, BigUint),
     e: BigUint,
     d: BigUint,
     pub n: BigUint,
@@ -36,7 +36,7 @@ impl RSA {
         // TODO: verify key length with log2
 
         RSA {
-            keys: (p1, p2),
+            // keys: (p1, p2),
             e: 65535.to_biguint().unwrap(),
             d,
             n,
@@ -55,26 +55,47 @@ impl RSA {
 
 impl Crypt for RSA {
     fn encrypt(&mut self, bytes: Vec<u8>) -> Vec<BigUint> {
+        // assert that the number of bytes is sufficient:
+        // e^message > n
+        // (2^16)^(message) > 2 ^ size
+        // 16 * message > size
+        // (2 ^ 4)*2(log2(bytes*8)) > size
+        // 4 + log2(bytes*8) > log2(size)
+        // 7 + log2(bytes) > log2(size)
+        // log2(bytes) > log2(size) - 7
+        // log2(bytes) > log2(size >> 7)
+        // bytes) > size >> 7
+
+        // TODO: instead of asserting, pad shit
+        assert!(bytes.len() > self.size >> 7);
         let (mut rest, mut data) =
             bytes
                 .iter()
                 .fold((Vec::new(), Vec::new()), |(mut rest, mut data), byte| {
                     rest.push(*byte);
                     if (rest.len() + 1) * 8 >= self.block_size() {
-                        data.push(self.encrypt_block(&BigUint::from_bytes_be(&mut rest.drain(0..(self.block_size() / 8)).collect::<Vec<u8>>()[..])));
+                        data.push(self.encrypt_block(&BigUint::from_bytes_be(
+                            &mut rest.drain(0..(self.block_size() / 8)).collect::<Vec<u8>>()[..],
+                        )));
                     }
                     (rest, data)
                 });
 
+        assert!(rest.len() > self.size >> 7);
+
         if rest.len() > 0 {
-            data.push(self.encrypt_block(&BigUint::from_bytes_be(&mut rest.drain(0..rest.len()).collect::<Vec<u8>>()[..])));
+            data.push(self.encrypt_block(&BigUint::from_bytes_be(
+                &mut rest.drain(0..rest.len()).collect::<Vec<u8>>()[..],
+            )));
         }
 
         data
     }
 
     fn decrypt(&mut self, nums: Vec<BigUint>) -> Vec<u8> {
-        nums.iter().flat_map(|elem| self.decrypt_block(elem).to_bytes_be()).collect()
+        nums.iter()
+            .flat_map(|elem| self.decrypt_block(elem).to_bytes_be())
+            .collect()
     }
 
     fn block_size(&self) -> usize {
@@ -96,7 +117,8 @@ mod tests {
         assert_eq!(decrypted, num.to_biguint().unwrap());
 
         // let string = b"hello motherfucker, hows life? is it good? i certanly hope so. if not, dont tell me".to_vec();
-        let string = b"iuha diuh diuh seouihafhj sfjkhbsvcuyb serufy bwuyebf ysbad ufy busyrbef uyawb uefybakjshdbf askjnbvyu ba yuefb aywebf hkjbcvuybwae fb kwaebyf uyabweuof bwoeyf owyuevfbuoy vacd habs kfjhwuyefbgo uyagfouywe gffhbwefyb aygrf oygwehab fhbwcyb ygrfv aygwerfhjwbe fsjdbc uybsdovgh hbwoauebyf oyuasgdvyb h r yuagrrf87a9 7y 0ra7h bhhas hdbvuyhasbdv ygawbhfnmabsd,nmbasvcbhudcb oghr8 gar jh".to_vec();
+        let string = b"iuha diuh diuh seouihafhj sfjkhbsvcuyb serufy bwuyebf ysbad ufy busyrbef uyawb uefybakjshdbf askjnbvyu ba yuefb aywebf hkjbcvuybwae fb kwaebyf uyabweuof bwoeyf owyuevfbuoy vacd habs kfjhwuyefbgo uyagfouywe gffhbwefyb aygrf oygwehab fhbwcyb ygrfv aygwerfhjwbe fsjdbc uybsdovgh hbwoauebyf oyuasgdvyb h r yuagrrf87a9 7y 0ra7h bhhas hdbvuyhasbdv ygawbhfnmabsd,nmbasvcbhudcb oghr8 gar jhioj".to_vec();
+        // let string = b"awdoiawjd".to_vec();
 
         let encrypted = keys.encrypt(string.clone());
 
@@ -111,5 +133,9 @@ mod tests {
         println!("{}", std::str::from_utf8(&decrypted[..]).expect("oh"));
 
         assert_eq!(decrypted, string);
+    }
+
+    fn big(x: u32) -> BigUint {
+        x.to_biguint().unwrap()
     }
 }
