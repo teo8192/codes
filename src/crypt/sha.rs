796388055d5c163c7956e1dd1e0d08b4dfdbf3e0
Teo <teodor@dahlknutsen.no>
2020-09-24 18:47:18 +0200

    The whole 512 family

--- src/crypt/sha.rs
+++ src/crypt/sha.rs
@@ -35,7 +35,7 @@ macro_rules! shr {
 
 macro_rules! ch {
     ($x:expr, $y:expr, $z:expr) => {{
-        ($x & $y) ^ (!($x as u64) & $z)
+        ($x & $y) ^ (!$x & $z)
     }};
 }
 
@@ -45,34 +45,34 @@ macro_rules! maj {
     }};
 }
 
-macro_rules! sum0256 {
+macro_rules! sum0512 {
     ($x:expr) => {{
         rotr!(28, $x) ^ rotr!(34, $x) ^ rotr!(39, $x)
     }};
 }
 
-macro_rules! sum1256 {
+macro_rules! sum1512 {
     ($x:expr) => {{
         rotr!(14, $x) ^ rotr!(18, $x) ^ rotr!(41, $x)
     }};
 }
 
-macro_rules! sigma0256 {
+macro_rules! sigma0512 {
     ($x:expr) => {{
         rotr!(1, $x) ^ rotr!(8, $x) ^ shr!(7, $x)
     }};
 }
 
-macro_rules! sigma1256 {
+macro_rules! sigma1512 {
     ($x:expr) => {{
         rotr!(19, $x) ^ rotr!(61, $x) ^ shr!(6, $x)
     }};
 }
 // }}}
 
-// {{{ The K cosntant
+// {{{ The K constant
 
-const K: [u64; 80] = [
+const K512: [u64; 80] = [
     0x428a2f98d728ae22, //  0
     0x7137449123ef65cd, //  1
     0xb5c0fbcfec4d3b2f, //  2
@@ -157,9 +157,50 @@ const K: [u64; 80] = [
 
 // }}}
 
-fn sha512(mut input: Vec<u8>) -> Box<[u8; 64]> {
-    #[allow(non_snake_case)]
-    let mut H = [
+macro_rules! create_box {
+    ($bytes:expr, $size:expr, $type:ty) => {{
+        let mut output = Box::new([0u8; $size >> 3]);
+
+        unsafe {
+            std::ptr::copy(
+                $bytes
+                    .iter()
+                    .fold(Vec::new(), |mut bytes, x| {
+                        for i in (0..(8 as $type)).rev() {
+                            bytes.push(((x >> (i << 3)) & 255) as u8)
+                        }
+
+                        bytes
+                    })
+                    .as_ptr(),
+                output.as_mut_ptr(),
+                $size >> 3,
+            );
+        }
+
+        output
+    }};
+}
+
+fn sha384(input: Vec<u8>) -> Box<[u8; 48]> {
+    let mut iv = [
+        0xcbbb9d5dc1059ed8,
+        0x629a292a367cd507,
+        0x9159015a3070dd17,
+        0x152fecd8f70e5939,
+        0x67332667ffc00b31,
+        0x8eb44a8768581511,
+        0xdb0c2e0d64f98fa7,
+        0x47b5481dbefa4fa4,
+    ];
+
+    sha512_base(input, &mut iv);
+
+    create_box!(iv, 384, u64)
+}
+
+fn sha512(input: Vec<u8>) -> Box<[u8; 64]> {
+    let mut iv = [
         0x6a09e667f3bcc908,
         0xbb67ae8584caa73b,
         0x3c6ef372fe94f82b,
@@ -169,7 +210,46 @@ fn sha512(mut input: Vec<u8>) -> Box<[u8; 64]> {
         0x1f83d9abfb41bd6b,
         0x5be0cd19137e2179,
     ];
+    sha512_base(input, &mut iv);
 
+    create_box!(iv, 512, u64)
+}
+
+fn sha512_256(input: Vec<u8>) -> Box<[u8; 32]> {
+    let mut iv = [
+        0x22312194FC2BF72C,
+        0x9F555FA3C84C64C2,
+        0x2393B86B6F53B151,
+        0x963877195940EABD,
+        0x96283EE2A88EFFE3,
+        0xBE5E1E2553863992,
+        0x2B0199FC2C85B8AA,
+        0x0EB72DDC81C52CA2,
+    ];
+
+    sha512_base(input, &mut iv);
+
+    create_box!(iv, 256, u64)
+}
+
+fn sha512_224(input: Vec<u8>) -> Box<[u8; 28]> {
+    let mut iv = [
+        0x8C3D37C819544DA2,
+        0x73E1996689DCD4D6,
+        0x1DFAB7AE32FF9C82,
+        0x679DD514582F9FCF,
+        0x0F6D2B697BD44DA8,
+        0x77E36F7304C48942,
+        0x3F9D85A86A1D36C8,
+        0x1112E6AD91D692A1,
+    ];
+
+    sha512_base(input, &mut iv);
+
+    create_box!(iv, 224, u64)
+}
+
+fn sha512_base(mut input: Vec<u8>, iv: &mut [u64; 8]) {
     let mut at_end = input.len() == 0;
     let mut added_one = false;
     let l = (input.len() as u128) << 3;
@@ -208,14 +288,14 @@ fn sha512(mut input: Vec<u8>) -> Box<[u8; 64]> {
             }
         }
 
-        let mut a = H[0];
-        let mut b = H[1];
-        let mut c = H[2];
-        let mut d = H[3];
-        let mut e = H[4];
-        let mut f = H[5];
-        let mut g = H[6];
-        let mut h: u64 = H[7];
+        let mut a = iv[0];
+        let mut b = iv[1];
+        let mut c = iv[2];
+        let mut d = iv[3];
+        let mut e = iv[4];
+        let mut f = iv[5];
+        let mut g = iv[6];
+        let mut h: u64 = iv[7];
 
         let mut w: Vec<u64> = Vec::new();
 
@@ -225,16 +305,16 @@ fn sha512(mut input: Vec<u8>) -> Box<[u8; 64]> {
                 m[t]
             } else {
                 sum!(
-                    sigma1256!(w[t - 2]),
+                    sigma1512!(w[t - 2]),
                     w[t - 7],
-                    sigma0256!(w[t - 15]),
+                    sigma0512!(w[t - 15]),
                     w[t - 16]
                 )
             };
             w[t] = next_w;
 
-            let t_1 = sum!(h, sum1256!(e), ch!(e, f, g), K[t], w[t]);
-            let t_2 = sum!(sum0256!(a), maj!(a, b, c));
+            let t_1 = sum!(h, sum1512!(e), ch!(e, f, g), K512[t], w[t]);
+            let t_2 = sum!(sum0512!(a), maj!(a, b, c));
 
             h = g;
             g = f;
@@ -244,36 +324,28 @@ fn sha512(mut input: Vec<u8>) -> Box<[u8; 64]> {
             c = b;
             b = a;
             a = sum!(t_1, t_2);
+
+//             if t == 79 {
+//                 println!("A/E: {:016X} {:016X}", a, e);
+//                 println!("B/F: {:016X} {:016X}", b, f);
+//                 println!("C/G: {:016X} {:016X}", c, g);
+//                 println!("D/H: {:016X} {:016X}", d, h);
+//             }
         }
 
-        H[0] = sum!(a, H[0]);
-        H[1] = sum!(b, H[1]);
-        H[2] = sum!(c, H[2]);
-        H[3] = sum!(d, H[3]);
-        H[4] = sum!(e, H[4]);
-        H[5] = sum!(f, H[5]);
-        H[6] = sum!(g, H[6]);
-        H[7] = sum!(h, H[7]);
+        iv[0] = sum!(a, iv[0]);
+        iv[1] = sum!(b, iv[1]);
+        iv[2] = sum!(c, iv[2]);
+        iv[3] = sum!(d, iv[3]);
+        iv[4] = sum!(e, iv[4]);
+        iv[5] = sum!(f, iv[5]);
+        iv[6] = sum!(g, iv[6]);
+        iv[7] = sum!(h, iv[7]);
+
+        // for i in iv.iter() {
+        //     println!("{:016X}", i);
+        // }
     }
-
-    let mut output = Box::new([0u8; 64]);
-
-    unsafe {
-        std::ptr::copy(
-            H.iter().fold(Vec::new(), |mut bytes, x| {
-                for i in (0..8u64).rev() {
-                    bytes.push(((x >> (i << 3)) & 255) as u8)
-                }
-
-                bytes
-            })[..]
-                .as_ptr(),
-            output.as_mut_ptr(),
-            64,
-        );
-    }
-
-    output
 }
 
 #[cfg(test)]
@@ -290,16 +362,14 @@ mod tests {
             0x2a, 0x9a, 0xc9, 0x4f, 0xa5, 0x4c, 0xa4, 0x9f,
         ];
 
-        let output = sha512b(b"abc".to_vec());
+        let output = sha512(b"abc".to_vec());
 
         assert_eq!(result[..], output[..]);
     }
 
     #[test]
     fn test_multiline_b() {
-        let input: Vec<u8> = (0x61..0x6f).flat_map(|i| i..(i + 8)).collect();
-
-        let hash = sha512b(input);
+        let hash = sha512((0x61..0x6f).flat_map(|i| i..(i + 8)).collect());
 
         let expected = [
             0x8E, 0x95, 0x9B, 0x75, 0xDA, 0xE3, 0x13, 0xDA, 0x8C, 0xF4, 0xF7, 0x28, 0x14, 0xFC,
@@ -310,5 +380,106 @@ mod tests {
         ];
 
         assert_eq!(hash[..], expected[..]);
+    }
+
+    #[test]
+    fn test_sha512_256() {
+        let input = b"Unde labore cumque quos iure aut ipsa. Voluptatem non explicabo voluptatem nesciunt pariatur vel. Aut asperiores molestiae quis.
+
+Ad animi nobis et aut et odio provident. Et quidem et molestiae quidem adipisci quia. Dolore molestiae ut excepturi debitis. Debitis vel qui laborum voluptas.
+
+Perferendis et et quis. Consequatur ad amet doloremque sint dolorem eligendi. At reiciendis suscipit similique sed nam neque consequatur. Qui et optio sequi occaecati. Deserunt id ab omnis ut rerum nobis consequatur.
+
+Accusantium corrupti dolor adipisci quisquam dolorum qui aut eos. Adipisci enim veritatis explicabo mollitia enim. Repellat aut perspiciatis a amet.
+
+Officiis et placeat alias voluptatem quasi non. Reiciendis qui quo mollitia occaecati. Molestiae iusto soluta voluptas quisquam vero a adipisci exercitationem. Consectetur harum sint ea. Distinctio et vero repellendus a.".to_vec();
+
+        let hash = sha512_256(input);
+
+        let expected = [
+            0x09, 0xa4, 0xa7, 0xff, 0x2e, 0xa4, 0x7b, 0xa4, 0x2d, 0xd0, 0x63, 0xf5, 0x5a, 0xde,
+            0x2e, 0x1f, 0x09, 0x30, 0x55, 0x74, 0x3c, 0x1e, 0x5b, 0x4c, 0x31, 0xf4, 0x3f, 0x13,
+            0x00, 0xf4, 0xe6, 0xa2,
+        ];
+
+        assert_eq!(expected[..], hash[..]);
+    }
+
+    #[test]
+    fn testsha512_256_nist_spec() {
+        let result: [u8; 32] = [
+            0x53, 0x04, 0x8E, 0x26, 0x81, 0x94, 0x1E, 0xF9, 0x9B, 0x2E, 0x29, 0xB7, 0x6B, 0x4C,
+            0x7D, 0xAB, 0xE4, 0xC2, 0xD0, 0xC6, 0x34, 0xFC, 0x6D, 0x46, 0xE0, 0xE2, 0xF1, 0x31,
+            0x07, 0xE7, 0xAF, 0x23,
+        ];
+
+        let output = sha512_256(b"abc".to_vec());
+
+        assert_eq!(result[..], output[..]);
+    }
+
+    #[test]
+    fn testsha512_256_nist_spec_long() {
+        let result: [u8; 32] = [
+            0x39, 0x28, 0xE1, 0x84, 0xFB, 0x86, 0x90, 0xF8, 0x40, 0xDA, 0x39, 0x88, 0x12, 0x1D,
+            0x31, 0xBE, 0x65, 0xCB, 0x9D, 0x3E, 0xF8, 0x3E, 0xE6, 0x14, 0x6F, 0xEA, 0xC8, 0x61,
+            0xE1, 0x9B, 0x56, 0x3A,
+        ];
+
+        let output = sha512_256((0x61..0x6f).flat_map(|i| i..(i + 8)).collect());
+
+        assert_eq!(result[..], output[..]);
+    }
+
+    #[test]
+    fn testsha384() {
+        let result: [u8; 48] = [
+            0xCB, 0x00, 0x75, 0x3F, 0x45, 0xA3, 0x5E, 0x8B, 0xB5, 0xA0, 0x3D, 0x69, 0x9A, 0xC6,
+            0x50, 0x07, 0x27, 0x2C, 0x32, 0xAB, 0x0E, 0xDE, 0xD1, 0x63, 0x1A, 0x8B, 0x60, 0x5A,
+            0x43, 0xFF, 0x5B, 0xED, 0x80, 0x86, 0x07, 0x2B, 0xA1, 0xE7, 0xCC, 0x23, 0x58, 0xBA,
+            0xEC, 0xA1, 0x34, 0xC8, 0x25, 0xA7,
+        ];
+
+        let output = sha384(b"abc".to_vec());
+
+        assert_eq!(result[..], output[..]);
+    }
+
+    #[test]
+    fn testsha384_long() {
+        let result: [u8; 48] = [
+            0x09, 0x33, 0x0C, 0x33, 0xF7, 0x11, 0x47, 0xE8, 0x3D, 0x19, 0x2F, 0xC7, 0x82, 0xCD,
+            0x1B, 0x47, 0x53, 0x11, 0x1B, 0x17, 0x3B, 0x3B, 0x05, 0xD2, 0x2F, 0xA0, 0x80, 0x86,
+            0xE3, 0xB0, 0xF7, 0x12, 0xFC, 0xC7, 0xC7, 0x1A, 0x55, 0x7E, 0x2D, 0xB9, 0x66, 0xC3,
+            0xE9, 0xFA, 0x91, 0x74, 0x60, 0x39,
+        ];
+
+        let output = sha384((0x61..0x6f).flat_map(|i| i..(i + 8)).collect());
+
+        assert_eq!(result[..], output[..]);
+    }
+
+    #[test]
+    fn testsha512_224() {
+        let result: [u8; 28] = [
+            0x46, 0x34, 0x27, 0x0F, 0x70, 0x7B, 0x6A, 0x54, 0xDA, 0xAE, 0x75, 0x30, 0x46, 0x08,
+            0x42, 0xE2, 0x0E, 0x37, 0xED, 0x26, 0x5C, 0xEE, 0xE9, 0xA4, 0x3E, 0x89, 0x24, 0xAA,
+        ];
+
+        let output = sha512_224(b"abc".to_vec());
+
+        assert_eq!(result[..], output[..]);
+    }
+
+    #[test]
+    fn testsha512_224_long() {
+        let result: [u8; 28] = [
+            0x23, 0xFE, 0xC5, 0xBB, 0x94, 0xD6, 0x0B, 0x23, 0x30, 0x81, 0x92, 0x64, 0x0B, 0x0C,
+            0x45, 0x33, 0x35, 0xD6, 0x64, 0x73, 0x4F, 0xE4, 0x0E, 0x72, 0x68, 0x67, 0x4A, 0xF9,
+        ];
+
+        let output = sha512_224((0x61..0x6f).flat_map(|i| i..(i + 8)).collect());
+
+        assert_eq!(result[..], output[..]);
     }
 }
