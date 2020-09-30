4dfb57c4a0687d1f9a96af9c38ff1e1053eaf160
Teo <teodor@dahlknutsen.no>
2020-09-30 14:35:58 +0200

    Added an example

---
+++ examples/cryptor.rs
@@ -0,0 +1,85 @@
+use codes::crypt::pbkdf2::pbkdf2;
+use codes::crypt::{AESKey, BlockCipher, AES};
+use codes::error::hamming::ErrorDetection;
+use std::fs::File;
+use std::io::{Read, Write};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+enum Mode {
+    #[structopt(name = "encrypt")]
+    Encrypt,
+    #[structopt(name = "decrypt")]
+    Decrypt,
+}
+
+#[derive(StructOpt)]
+#[structopt(name = "cryptor", about = "Simple CLI encryption tool.")]
+struct Cli {
+    #[structopt(short, long)]
+    input: Option<String>,
+    #[structopt(short, long)]
+    output: Option<String>,
+    #[structopt(short, long)]
+    password: String,
+    #[structopt(subcommand)]
+    mode: Mode,
+}
+
+fn run(args: Cli) -> Result<(), std::io::Error> {
+    let mut bytes = Vec::new();
+
+    // get password up/down to 256 bit length
+    // TODO: better IV creation
+    let key_vec = pbkdf2(
+        args.password.bytes().collect(),
+        (0..16).rev().collect(),
+        10000,
+        256,
+    );
+    assert_eq!(key_vec.len(), 32);
+    let mut key = [0u8; 32];
+    unsafe {
+        std::ptr::copy(key_vec.as_ptr(), key.as_mut_ptr(), key_vec.len());
+    }
+
+    // read input, either from file or from stdin
+    if let Some(filename) = args.input {
+        File::open(filename)?.read_to_end(&mut bytes)?;
+    } else {
+        std::io::stdin().read_to_end(&mut bytes)?;
+    }
+
+    let iv = (0..16).collect();
+
+    // initialize the encryption
+    let aes = AES::new(AESKey::AES256(key));
+
+    let data = match args.mode {
+        Mode::Encrypt => {
+            aes.cbc_encrypt(&iv, &mut bytes).unwrap();
+            bytes.into_iter().encode().collect()
+        }
+        Mode::Decrypt => {
+            bytes = bytes.into_iter().decode().collect();
+            while bytes.len() & 15 != 0 {
+                bytes.pop();
+            }
+            aes.cbc_decrypt(&iv, &mut bytes).unwrap();
+            bytes
+        }
+    };
+
+    // write the processed data to either a file or to stdout
+    if let Some(filename) = args.output {
+        File::create(filename)?.write(&data)
+    } else {
+        std::io::stdout().write(&data)
+    }?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), std::io::Error> {
+    run(Cli::from_args())
+}
